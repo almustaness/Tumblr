371c93fb13c4bcad39421ac5aed471267ff97b0c
almustaness <e-comm@live.com>
2016-08-18 09:14:14 +0000

    We created show, new, and index actions for posts

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+
+
+ActiveRecord::Schema.define(version: 20160818082301) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
